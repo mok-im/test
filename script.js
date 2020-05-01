69c9b05f35df4e7d6c313b44a2af00b22311f396
Igor <igorflyiq442@gmail.com>
2020-05-01 14:42:01 +0300

    это наш первый комит

---
+++ script.js
@@ -0,0 +1,35 @@
+let persone = {
+    name:"Igor",
+    age: 25,
+    isMarried: false,
+};
+
+console.log (persone.age);
+
+// massive
+// let mass = ["mass1","mass2","mass3"];
+
+// console.log(mass[2]);
+
+// Confirm
+// let answer = confirm("Are U ready ?");
+
+// console.log(answer);
+
+// Promt
+
+// let answer = prompt("Are U ready ?", "Yes");
+
+// console.log(answer);
+
+// Інкремент
+
+let incr=5,
+    decr=8;
+
+incr++;
+decr--;
+
+console.log(incr,decr);
+
+console.log(22%2);
